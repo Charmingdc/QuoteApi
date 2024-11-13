0650f4c88eb30481a5806219e4c7262fbd0bbe4b
Charmingdc <charmingdc002@gmail.com>
2024-11-13 12:09:34 +0100

    update

--- api/quotes.js
+++ api/quotes.js
@@ -1,16 +1,5 @@
-'use stict';
-
-
-import fs from 'fs';
-import path from 'path';
+import quotes from './quotes.json';
 
 export default function handler(req, res) {
-  const filePath = path.join(process.cwd(), 'quotes.json');
-  fs.readFile(filePath, 'utf8', (err, data) => {
-    if (err) {
-      res.status(500).json({ error: 'Could not read file' });
-      return;
-    }
-    res.status(200).json(JSON.parse(data));
-  });
+  res.status(200).json(quotes);
 }
