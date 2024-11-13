b66aab24127a85cb01d777ad9a6b72edfb3b3f7f
Charmingdc <charmingdc002@gmail.com>
2024-11-13 13:28:22 +0100

    update quotes.js

--- api/quotes.js
+++ api/quotes.js
@@ -1,5 +1,8 @@
 import quotes from '../quotes.json';
 
 export default function handler(req, res) {
+  // Allow requests from any origin
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Content-Type', 'application/json');
   res.status(200).json(quotes);
 }
